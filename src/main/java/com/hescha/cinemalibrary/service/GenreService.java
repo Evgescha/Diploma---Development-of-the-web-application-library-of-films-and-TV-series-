ce89b0e0bba899afb5f49940711b225267b98d13
Yauhescha <evgescha.maslov007@yandex.ru>
2023-04-23 19:21:21 +0300

    remove genre

--- src/main/java/com/hescha/cinemalibrary/service/GenreService.java
+++ src/main/java/com/hescha/cinemalibrary/service/GenreService.java
@@ -1,7 +1,9 @@
 package com.hescha.cinemalibrary.service;
 
 import com.hescha.cinemalibrary.model.Genre;
+import com.hescha.cinemalibrary.model.Item;
 import com.hescha.cinemalibrary.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,9 @@ import java.util.Set;
 public class GenreService extends CrudService<Genre> {
 
     private final GenreRepository repository;
+    @Autowired
+    private ItemRepository itemRepository;
+
 
     public GenreService(GenreRepository repository) {
         super(repository);
@@ -38,5 +43,25 @@ public class GenreService extends CrudService<Genre> {
 
     private void updateFields(Genre entity, Genre read) {
         read.setName(entity.getName());
+    }
+
+    @Override
+    public void delete(long id) {
+        // Найти жанр по идентификатору
+        Genre genre = repository.findById(id).orElse(null);
+
+        if (genre != null) {
+            // Найти все элементы, связанные с жанром
+            List<Item> itemsWithGenre = itemRepository.findByGenresContains(genre);
+
+            // Удалить жанр из списка жанров каждого элемента
+            for (Item item : itemsWithGenre) {
+                item.getGenres().remove(genre);
+                itemRepository.save(item);
+            }
+
+            // Удалить жанр из базы данных
+            repository.deleteById(id);
+        }
     }
 }
